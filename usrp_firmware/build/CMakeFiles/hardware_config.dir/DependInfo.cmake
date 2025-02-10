4a02cc730933e98c5f86d296dd5d928147bfbc80
roryschram <66696753+roryschram@users.noreply.github.com>
2025-02-10 16:29:55 +0200

    built on ubuntu machine

--- usrp_firmware/build/CMakeFiles/hardware_config.dir/DependInfo.cmake
+++ usrp_firmware/build/CMakeFiles/hardware_config.dir/DependInfo.cmake
@@ -1,23 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
-  )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/usrp_firmware/src/hardware/RX_Funcs.cpp" "/home/usrp_firmware/build/CMakeFiles/hardware_config.dir/src/hardware/RX_Funcs.cpp.o"
-  "/home/usrp_firmware/src/hardware/RX_config.cpp" "/home/usrp_firmware/build/CMakeFiles/hardware_config.dir/src/hardware/RX_config.cpp.o"
-  "/home/usrp_firmware/src/hardware/TX_Funcs.cpp" "/home/usrp_firmware/build/CMakeFiles/hardware_config.dir/src/hardware/TX_Funcs.cpp.o"
-  "/home/usrp_firmware/src/hardware/TX_config.cpp" "/home/usrp_firmware/build/CMakeFiles/hardware_config.dir/src/hardware/TX_config.cpp.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
-
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "../inc"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/usrp/Desktop/masters/usrp_firmware/src/hardware/RX_Funcs.cpp" "CMakeFiles/hardware_config.dir/src/hardware/RX_Funcs.cpp.o" "gcc" "CMakeFiles/hardware_config.dir/src/hardware/RX_Funcs.cpp.o.d"
+  "/home/usrp/Desktop/masters/usrp_firmware/src/hardware/RX_config.cpp" "CMakeFiles/hardware_config.dir/src/hardware/RX_config.cpp.o" "gcc" "CMakeFiles/hardware_config.dir/src/hardware/RX_config.cpp.o.d"
+  "/home/usrp/Desktop/masters/usrp_firmware/src/hardware/TX_Funcs.cpp" "CMakeFiles/hardware_config.dir/src/hardware/TX_Funcs.cpp.o" "gcc" "CMakeFiles/hardware_config.dir/src/hardware/TX_Funcs.cpp.o.d"
+  "/home/usrp/Desktop/masters/usrp_firmware/src/hardware/TX_config.cpp" "CMakeFiles/hardware_config.dir/src/hardware/TX_config.cpp.o" "gcc" "CMakeFiles/hardware_config.dir/src/hardware/TX_config.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
--- usrp_firmware/build/CMakeFiles/utility_library.dir/DependInfo.cmake
+++ usrp_firmware/build/CMakeFiles/utility_library.dir/DependInfo.cmake
@@ -1,20 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
-  )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/usrp_firmware/src/utilities/config_reader.cpp" "/home/usrp_firmware/build/CMakeFiles/utility_library.dir/src/utilities/config_reader.cpp.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
-
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "../inc"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/usrp/Desktop/masters/usrp_firmware/src/utilities/config_reader.cpp" "CMakeFiles/utility_library.dir/src/utilities/config_reader.cpp.o" "gcc" "CMakeFiles/utility_library.dir/src/utilities/config_reader.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
