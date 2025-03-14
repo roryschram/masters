21c9d420fa27ee085a7761f979adb4be360c6147
roryschram <66696753+roryschram@users.noreply.github.com>
2025-03-14 15:33:57 +0200

    getting docker working, recopiled

--- usrp_firmware/build/CMakeFiles/processing_library.dir/cmake_clean.cmake
+++ usrp_firmware/build/CMakeFiles/processing_library.dir/cmake_clean.cmake
@@ -1,6 +1,5 @@
 file(REMOVE_RECURSE
   "CMakeFiles/processing_library.dir/src/processing/DSP.cpp.o"
-  "CMakeFiles/processing_library.dir/src/processing/DSP.cpp.o.d"
   "libprocessing_library.a"
   "libprocessing_library.pdb"
 )
