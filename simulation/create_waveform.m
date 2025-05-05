8c70c81329247b70b8f4e391bf8737585450717f
Rory Schram <schror002@myuct.ac.za>
2025-05-05 16:44:16 +0200

    cleaning up

--- simulation/create_waveform.m
+++
@@ -1,5 +0,0 @@
-sig = chirp(1000, 10000, 0.01, 20000, 1);  
-I = real(sig);  
-Q = imag(sig);  
-hdf5write('chirp.h5', '/I/value', I, '/Q/value', Q);  
-plot(sig)
--- simulation/read_waveform.m
+++
@@ -1,7 +0,0 @@
-[Iin Qin scale] = loadfersHDF5('Monostatic.h5');  
-I_Q_scaled = (Iin + j*Qin)*scale; 
-matched = conv(I_Q_scaled,sig); % perform "matched filtering"  
-figure(1) %plot output of matched filter  
-magnitude = sqrt( imag(matched).^2 + real(matched.^2) ); % get magnitude of complex signal  
-plot(magnitude(length(sig):length(magnitude))) % ignore last bit (overlap of convolution)  
-hold on 
