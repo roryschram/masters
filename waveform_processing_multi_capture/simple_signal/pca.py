c8f82d852b176545153d26441d8a34825c6635a4
Rory Schram <schror002@myuct.ac.za>
2025-05-28 16:36:07 +0200

    got svm working

--- waveform_processing_multi_capture/simple_signal/pca.py
+++ waveform_processing_multi_capture/simple_signal/pca.py
@@ -1,20 +1,17 @@
 import matplotlib.pyplot as plt
-# plt.rcParams['figure.figsize'] = (10, 4)
-# plt.rcParams['figure.dpi'] = 300
-
 import numpy as np
-import scipy
-import h5py
-
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
-from scipy.signal import decimate
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report, confusion_matrix
+from mpl_toolkits.mplot3d import Axes3D
 
 channel_ests = []
 
 fig, axs = plt.subplots()
 
-for i in range(1,251,1):
+for i in range(1,201,1):
     input = np.load("../masters_large_data/received_data/multi_receive/channel_estimations/channel_est"+str(i)+".npy")
     channel_ests.append(input)
     axs.plot(input,color='orange')
@@ -27,27 +24,19 @@ plt.show()
 
 
 # Parameters
-num_captures = 250
+num_captures = 200
 window_size =  320 # We'll use the first 8192 samples of each
 labels = (
     ['First'] * 50 + 
     ['Second'] * 50 +
     ['Third'] * 50 +
-    ['Fourth'] * 50 +
-    ['Fifth'] * 50
+    ['Fourth'] * 50
 )
 
+label_to_int = {'First': 0, 'Second': 1, 'Third': 2, 'Fourth': 3}
+int_labels = np.array([label_to_int[label] for label in labels])
+
 X = []
-
-# # Extract FFT magnitude features from each variable
-# for i in range(1, num_captures + 1):
-#     capture = globals()[f"capture{i}"][:window_size]  # Access variable by name
-#     # Downsample to 12.5k samples
-#     downsample_factor = 12500
-#     capture_ds = decimate(capture, downsample_factor)
-#     X.append(capture_ds)
-
-
 X = np.array(channel_ests)
 
 
@@ -61,11 +50,33 @@ X_pca = pca.fit_transform(X_scaled)
 
 print(pca.explained_variance_ratio_)
 
+
+
+
+# Split into train/test sets
+X_train, X_test, y_train, y_test = train_test_split(X_pca, int_labels, test_size=0.5, random_state=42, stratify=int_labels)
+
+# Train SVM classifier
+svm = SVC(kernel='rbf', C=1.0, gamma='scale')
+svm.fit(X_train, y_train)
+
+# Predict and evaluate
+y_pred = svm.predict(X_test)
+
+
+print("Classification Report:")
+print(classification_report(y_test, y_pred, target_names=label_to_int.keys()))
+print("Confusion Matrix:")
+print(confusion_matrix(y_test, y_pred))
+
+
+
+
 # Plotting
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 
-color_map = {'First': 'r', 'Second': 'g','Third': 'b','Fourth': 'y','Fifth': 'c'}
+color_map = {'First': 'r', 'Second': 'g','Third': 'b','Fourth': 'y'}
 
 for i in range(len(X_pca)):
     ax.scatter(X_pca[i, 0], X_pca[i, 1], X_pca[i, 2],
@@ -75,7 +86,7 @@ for i in range(len(X_pca)):
 ax.set_xlabel('PC1')
 ax.set_ylabel('PC2')
 ax.set_zlabel('PC3')
-ax.set_title("PCA of Channel Estimations")
+ax.set_title("PCA Space")
 # ax.view_init(elev=, azim=0)  # ← change perspective to top-down
 ax.legend()
 plt.show()
