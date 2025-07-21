7ddba2f0101459d9e78f4b41083dfab7a24b0b8c
Rory Schram <schror002@myuct.ac.za>
2025-07-21 15:37:28 +0200

    Made pca examination easier to program

--- final_testing/classification_testing/pca.py
+++ final_testing/classification_testing/pca.py
@@ -7,11 +7,26 @@ from sklearn.svm import SVC
 from sklearn.metrics import classification_report, confusion_matrix
 from mpl_toolkits.mplot3d import Axes3D
 
+
+# Settings for PCA
+
+# Number of captures to ingest
+ingest_number = 150
+
+# Number of captures per target
+captures_per_target = 50
+
+# Labels
+labels_pre = ['1st','2nd','3rd']
+
+
+
+
+
 channel_ests = []
 
-# fig, axs = plt.subplots()
 
-for i in range(1,151,1):
+for i in range(1,ingest_number+1,1):
     input = np.load("../masters_large_data/final_testing/classification_testing/channel_ests/channel_est"+str(i)+".npy")
     channel_ests.append(np.abs(input))
 #     axs.plot(np.abs(input))
@@ -22,13 +37,18 @@ for i in range(1,151,1):
 # plt.show()
 
 
-labels = (
-    ['1st'] * 50 + 
-    ['2nd'] * 50 +
-    ['3rd'] * 50
-)
+labels = []
+for i in labels_pre:
+    for k in range(0,captures_per_target):
+        labels.append(i)
 
-label_to_int = {'1st': 0, '2nd': 1, '3rd': 2}
+# labels = (
+#     ['1st'] * captures_per_target + 
+#     ['2nd'] * captures_per_target +
+#     ['3rd'] * captures_per_target
+# )
+
+label_to_int = {label: idx for idx, label in enumerate(labels_pre)}
 int_labels = np.array([label_to_int[label] for label in labels])
 
 X = []
@@ -43,7 +63,7 @@ X_scaled = scaler.fit_transform(X)
 pca = PCA(n_components=20)
 X_pca = pca.fit_transform(X_scaled)
 
-print(pca.explained_variance_ratio_)
+# print(pca.explained_variance_ratio_)
 
 
 
@@ -71,12 +91,20 @@ print(confusion_matrix(y_test, y_pred))
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 
-color_map = {'1st': 'r', '2nd': 'g', '3rd': 'b'}
+# Define a list of color codes (as many as you need)
+colors = ['r', 'g', 'b', 'c', 'm', 'y', 'k']  # red, green, blue, cyan, magenta, yellow, black
+
+# Get sorted unique labels (or just unique if you don’t care about order)
+unique_labels = labels_pre
+
+# Create the color map
+color_map = {label: colors[idx] for idx, label in enumerate(unique_labels)}
+
 
 for i in range(len(X_pca)):
     ax.scatter(X_pca[i, 0], X_pca[i, 1], X_pca[i, 2],
                color=color_map[labels[i]],
-               label=labels[i] if i % 50 == 0 else "")  # Avoid repeated labels
+               label=labels[i] if i % captures_per_target == 0 else "")  # Avoid repeated labels
 
 ax.set_xlabel('PC1')
 ax.set_ylabel('PC2')
