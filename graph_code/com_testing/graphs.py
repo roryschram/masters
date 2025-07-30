b2f153d1073fb5ca9bcf2555f85bfdc1da9a7fa9
roryschram <66696753+roryschram@users.noreply.github.com>
2025-07-30 17:02:18 +0200

    Did some report work

--- graph_code/com_testing/graphs.py
+++ graph_code/com_testing/graphs.py
@@ -5,9 +5,19 @@ import pandas as pd
 
 df = pd.read_csv("graph_code/com_testing/loopback.csv")
 
+attenuation = df["Attenuation"].astype(float).to_numpy()
+snr = df["avg_snr_dB"].astype(float).to_numpy()
+ber = df["avg_BER"].astype(float).to_numpy()
 
-plt.plot(df["Attenuation"],df["avg_BER"])
-plt.scatter(df["Attenuation"][::2],df["avg_BER"][::2])
+print(plt.style.available)
+plt.style.use('seaborn-v0_8')  # or 'ggplot', 'classic', etc.
+plt.figure(figsize=(10, 6))
+plt.plot(attenuation,ber)
+plt.scatter(attenuation[::1],ber[::1])
+
+
+for x,y,snr in zip(attenuation[::1],ber[::1],snr[::1]):
+    plt.text(x,y,str(snr))
 
 
 plt.xlabel("Attenuation (dB)")
@@ -15,4 +25,8 @@ plt.gca().invert_xaxis()
 
 
 plt.ylabel("BER (%)")
+plt.title("Graph: To show the relationship between attenuation of a communication signal\nand the bit error rate achived for a loopback test")
+
+plt.tight_layout()
+plt.savefig("test.png",dpi=300)
 plt.show()
